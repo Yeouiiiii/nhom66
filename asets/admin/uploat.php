1c80c886df26af4cad7b9cb5b2e8a069eb87adff
your name <hungthai7b@gmail.com>
2024-11-08 11:02:22 +0700

    aa

---
+++ asets/admin/uploat.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+<html>
+    <header>
+
+    </header>
+    <body>
+        <h1>uploat file</h1>
+        <form action="uloat file " method="post" enctype="multipart\form-data">
+            <input type="file" name="file_uploat">
+            <input type="submin" value="Uploat" name=""uploat>
+        </form>
+    </body>
+</html>
